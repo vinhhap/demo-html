c478dee6b13e91b989fb7e800a116583caa9e659
Quan <qnguyen341996@gmail.com>
2016-01-26 15:40:16 +0000

    Vong lap

--- javascript/vonglap/Baitap.js
+++ javascript/vonglap/Baitap.js
@@ -6,7 +6,7 @@ var giaiThua = function(n) {
   }
   return kq;
 };
-console.log(giaiThua(5));
+console.log(giaiThua(3));
 
 //Bai Tap 2
 var daoNguoc = function(str) {
@@ -17,4 +17,42 @@ var daoNguoc = function(str) {
   kq.reverse();
   return kq.join('');
 };
-console.log(daoNguoc("hello"));
+console.log(daoNguoc("quan"));
+
+//Bai Tap 3
+var duplicate = function(str) {
+  kq = "";
+  for(var i = 0; i < 10; i++) {
+    kq += str + "-";
+  }
+  return kq.substring(0, kq.length - 1);
+};
+console.log(duplicate("123"));
+
+//Bai Tap 4
+var sapXep = function(hoc_vien) {
+  kq = hoc_vien.sort();
+  for(var i = 0; i < hoc_vien.length; i++) {
+    console.log(kq[i]);
+  }
+};
+console.log(sapXep(["Nguyen Nam Quan", "Quan Nam Nguyen", "Nam Nguyen Quan"]));
+
+//Bai Tap 5
+var gapDoi = function(num_arr) {
+  for(var i = 0; i < num_arr.length; i++) {
+    num_arr[i] *= 2;
+  }
+  return num_arr;
+}
+console.log(gapDoi([1,2,3]));
+
+//Bai Tap 6
+var soDu = function(num_arr) {
+  kq = [];
+  for(var i = 0; i < num_arr.length; i++) {
+    kq.push(num_arr[i] % 2);
+  }
+  return kq;
+}
+console.log(soDu([10, 3, 4, 5]));
